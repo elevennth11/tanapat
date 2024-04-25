37419e3fd1b65c7201587e7843a94e5ad4dfe603
NesJaaTH <nestv001@gmail.com>
2024-04-26 00:21:24 +0700

    updata 332

--- S_332.py
+++ S_332.py
@@ -1,90 +1,76 @@
 import socket , pickle , random , sys
 from datetime import datetime
-import random
-
-def capitalize_name(nickname):
-    return nickname.capitalize()
-
 port = 9876
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("", port))
-server_socket.listen(1)
-print("Server is listening on port", port)
-
-correct_password = "332"
-
+passwordserver = "332"
 temp = 0
 humi = 0
 pump_status = "off"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("", port))
+s.listen(5)
+
+print("Server is listening on port", port)
 
 try:
-
     while True:
-        client, address = server_socket.accept()
-        
-        data, server_address = server_socket.recv(4096)
-        command = data.decode().lower()
-        if command == "passwordformclient":
-            response = correct_password
-            server_socket.send(response.encode(), server_address)
-        data1, server_address1 = server_socket.recv(4096)
-        data_arr = pickle.loads(data1)
-        if data_arr[0] == "connect to server":
+        clientsocket, address = s.accept()
+        msg = clientsocket.recv(4096).decode("utf-8")
+        if msg == "passwordformclient":
+            clientsocket.sendall(str.encode(passwordserver))
+        msg1 = clientsocket.recv(4096)
+        data_pack = pickle.loads(msg1)
+        if data_pack[0] == "connect to server":
+            print(f"Hello {address[0]} : {address[1]}")
             datetime_now = datetime.now()
-            print(f"Hello {server_address1[0]} : {server_address1[1]}")
-            response = f"Hello {data_arr[1][0].upper()+data_arr[1][1:].lower()} [{datetime_now.ctime()}] "
-            server_socket.send(response.encode(), server_address1)
-        while 1:
-            data2, server_address2 = server_socket.recv(4096)
-            if data2.decode().lower() == "temp":
+            response = f"Hello {data_pack[1][0].upper()+data_pack[1][1:].lower()} [{datetime_now.ctime()}] "
+            clientsocket.sendall(str.encode(response))
+        while 1 :
+            msg = clientsocket.recv(4096).decode("utf-8")
+            if msg.lower() == "temp":
                 header = "temp"
-                tempram = random.randrange(50)
-                temp = tempram
-                response = ([header, tempram])
+                temprandom = random.randrange(50)
+                temp = temprandom
+                response = [header, temp]
                 data_pack = pickle.dumps(response)
-                print(f"Temp send {tempram}")
-                server_socket.send(data_pack, server_address2)
-            elif data2.decode().lower() == "humi":
+                print(f"Temp Send {temp}")
+                clientsocket.sendall(data_pack)
+            elif msg.lower() == "humi":
                 header = "humi"
-                humiram = random.randrange(100)
-                humi = humiram
-                if humiram >= 50:
+                humirandom = random.randrange(50)
+                humi = humirandom
+                if humirandom >= 50:
                     pump_status = "off"
-                elif humiram <= 50:
+                else:
                     pump_status = "on"
-                response = ([header, humiram])
+                response = [header, humi]
                 data_pack = pickle.dumps(response)
-                print(f"Humidity send {humiram}")
-                server_socket.send(data_pack, server_address2)
-            elif data2.decode().lower() == "all":
+                print(f"Humi Send {humi}")
+                clientsocket.sendall(data_pack)
+            elif msg.lower() == "all":
                 header = "all"
                 datetime_now = datetime.now()
-                response = ([header, datetime_now.ctime(), temp, humi, pump_status])
-                data_pack = pickle.dumps(response)
-                print(f"All data send {temp} {humi} {pump_status}")
+                response = [header, datetime_now.ctime(), temp, humi, pump_status]
                 print(f"All data: {datetime_now.ctime()} Temp: {temp} Humi : {humi} PUMP STATUS : {pump_status}")
-                server_socket.send(data_pack, server_address2)
-            elif data2.decode().lower() == "exit":
+                data_pack = pickle.dumps(response)
+                clientsocket.sendall(data_pack)
+            elif msg.lower() == "exit":
                 header = "exit"
                 response = ([header,"Confirm exit (y/n): "])
                 data_pack = pickle.dumps(response)
-                server_socket.send(data_pack, server_address2)
-                data2, server_address2 = server_socket.recv(4096)
-                if data2.decode().lower() == "y":
+                clientsocket.sendall(data_pack)
+                msgcon = clientsocket.recv(4096).decode("utf-8")
+                if msgcon.lower() == "y":
                     print(".....Bye Client.....")
                     sys.exit()
             else:
                 header = "nothing"
-                response = ([header, data2.decode()])
+                response = ([header, msg])
                 data_pack = pickle.dumps(response)
-                server_socket.send(data_pack, server_address2)
-        server_socket.close()
-
-
+                clientsocket.sendall(data_pack)
+        s.close()
 except Exception as e:
     print("An error occurred:", e)
 
 finally:
     print('closing socket')
-    server_socket.close()
+    s.close()
